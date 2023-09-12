93dd149d91eb311be534ee9577ccd5c9a4065168
Rithvij <rithvij315@gmail.com>
2023-09-12 15:50:12 -0700

    finishing up

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,2 @@
 module github.com/TutorialEdge/realtime-chat-go-react
-
-go 1.21.1
-
-require github.com/gorilla/websocket v1.5.0 // indirect
+require github.com/gorilla/websocket v1.4.0 
--- backend/main.go
+++ backend/main.go
@@ -7,21 +7,33 @@ import (
 	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
 
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Simple Server")
-	// })
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "running...")
+	})
 }
 
 func main() {
